9c75d0855b36404a61d319e4881974614ddee063
Lucas Santos <lucas.souza_santos@live.com>
2023-05-26 14:23:25 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -7,15 +7,35 @@ function cadastrocursos() {
     const nasc = new Date(idade);
     let idadeconver = hoje.getFullYear() - nasc.getFullYear();
 
+    matricula = Math.floor(Math.random() * 1000000);
+
     rendapercapita = (renda / pessoas)
 
     if (idadeconver >= 16 && rendapercapita <= 1.5) {
-        document.getElementById('mostrarcurso').innerHTML = ('Bem Vindo ao PSG. (0800-777-3622)');
+        document.getElementById('mostrarcurso').innerHTML = (`Bem Vindo ao PSG. (0800-777-3622) Sua Matricula e: ${matricula}`);
+        document.getElementById('mostrarcurserro').innerHTML = ('');
+        document.getElementById('mostrarcursoidade').innerHTML = ('');
+        document.getElementById('mostrarcursopreencher').innerHTML = ('');
     } else if (idadeconver < 16) {
-        document.getElementById('mostrarcurso').innerHTML = ('Você Não Tem Idade Suficiente.')
+        document.getElementById('mostrarcurserro').innerHTML = ('');
+        document.getElementById('mostrarcursopreencher').innerHTML = ('');
+        document.getElementById('mostrarcurso').innerHTML = ('');
+        let minhaDiv = document.getElementById('mostrarcursoidade');
+        minhaDiv.style.backgroundColor = '#059b9a';
+        document.getElementById('mostrarcursoidade').innerHTML = ('Você Não Tem Idade Suficiente.');
     } else if (rendapercapita > 1.5) {
-        document.getElementById('mostrarcurso').innerHTML = ('Você Ultrapassou O Limite Da Renda Per Capita.')
+        document.getElementById('mostrarcursoidade').innerHTML = ('');
+        document.getElementById('mostrarcursopreencher').innerHTML = ('');
+        document.getElementById('mostrarcurso').innerHTML = ('');
+        let minhaDiv = document.getElementById('mostrarcurserro');
+        minhaDiv.style.backgroundColor = '#ff4a4a';
+        document.getElementById('mostrarcurserro').innerHTML = ('Você Ultrapassou O Limite Da Renda Per Capita.')
     } else {
-        document.getElementById('mostrarcurso').innerHTML = ('Preencha Algum Campo.')
+        document.getElementById('mostrarcursoidade').innerHTML = ('');
+        document.getElementById('mostrarcurserro').innerHTML = ('');
+        document.getElementById('mostrarcurso').innerHTML = ('');
+        let minhaDiv = document.getElementById('mostrarcursopreencher');
+        minhaDiv.style.backgroundColor = '#e0d124';
+        document.getElementById('mostrarcursopreencher').innerHTML = ('Preencha Algum Campo.')
     }
 }
